4101d0148a243b415488bca31fa2cda6a278a58a
johmert <johmert@users.noreply.github.com>
2022-01-17 14:15:44 -0800

    initial commit

---
+++ src/user/User.js
@@ -0,0 +1,74 @@
+import React, { useEffect, useState } from "react";
+import { BrowserRouter as Route, Link, Switch, useParams, useRouteMatch } from "react-router-dom";
+import UserProfile from "./UserProfile";
+import { fetchUserWithPosts } from "../api";
+import PostList from "./PostList";
+import PostsNav from "./PostsNav";
+import ErrorMessage from "../common/ErrorMessage";
+
+export const User = () => {
+  const [user, setUser] = useState({ posts: [] });
+  const [error, setError] = useState(undefined);
+  const userId = useParams().userId;
+  const {path} = useRouteMatch();
+
+  useEffect(() => {
+    const abortController = new AbortController();
+    fetchUserWithPosts(userId, abortController.signal)
+      .then(setUser)
+      .catch(setError);
+
+    return () => abortController.abort();
+  }, [userId]);
+
+  if (error) {
+    return (
+      <ErrorMessage error={error}>
+        <p>
+          <Link to="/"> Go Home </Link>
+        </p>
+      </ErrorMessage>
+    );
+  }
+
+  /*
+    TODO: In the below section, update the links to work appropriately with React Router.
+
+    TODO: You'll need to add nested routes below.
+
+    The <PostList /> component should show on the following route:
+    /users/:userId/posts
+
+    The <UserProfile /> component should show on the following route:
+    /users/:userId
+  */
+  return (
+    <section className="container">
+      <PostsNav />
+      <div className="border p-4 h-100 d-flex flex-column">
+        <h2 className="mb-3">{user.name}</h2>
+        <ul className="nav nav-tabs">
+          <li className="nav-item">
+            <Link to={`${path}`} className="nav-link">Profile</Link>
+          </li>
+          <li className="nav-item">
+            <Link to={`${path}/posts`} className="nav-link">Posts</Link>
+          </li>
+        </ul>
+
+        {user.id ? (
+          <div className="p-4 border border-top-0">
+            <PostList posts={user.posts} />
+            <UserProfile user={user} />
+          </div>
+        ) : (
+          <div className="p-4 border border-top-0">
+            <p>Loading...</p>
+          </div>
+        )}
+      </div>
+    </section>
+  );
+};
+
+export default User;

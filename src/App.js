3c347129c912c75e89fc6fba645aebef121f6379
johmert <johmert@users.noreply.github.com>
2022-01-17 14:28:05 -0800

    App & Card completed

--- src/App.js
+++ src/App.js
@@ -5,25 +5,16 @@ import CardList from "./home/CardList";
 import User from "./user/User";
 import NotFound from "./common/NotFound"
 function App() {
-  /*
-    TODO: There is no need to add a <Router >, it is in index.js.
-
-    The <CardList /> component should be shown when the user is on the index path.
-
-    The <User /> component should be shown when the user is on the following path:
-    /users/:userId
-
-    Display <NotFound /> when appropriate
-  */
+  
   return (
     <Fragment>
       <Header />
       <Switch>
         <Route exact path="/">
-          <User />
+          <CardList />
         </Route>
         <Route path="/users/:userId">
-          <CardList />
+          <User />
         </Route>
         <Route>
           <NotFound />
--- src/home/Card.js
+++ src/home/Card.js
@@ -1,17 +1,13 @@
 import React from "react";
-
-/*
-  TODO: Change the link below to go to the user route, using the user's ID.
-  /users/:userId
-*/
+import { BrowserRouter as Link } from "react-router-dom";
 
 export const Card = ({ user = { posts: [] } }) => (
   <article className="col-12 col-md-6 col-xl-3 my-2 align-self-stretch">
     <div className="border p-4 h-100 d-flex flex-column">
       <h2 className="font-weight-lighter flex-fill">
-        <a>{user.name}</a>
+        <Link to="/users/:userId">{user.name}</Link>
       </h2>
-      <a className="mt-2">{user.posts.length} Posts »</a>
+      <Link to="/users/:userId/posts" className="mt-2">{user.posts.length} Posts »</Link>
     </div>
   </article>
 );
